d66e6a2942e5456093a06c2e86429af89a047836
jamirkhan6 <ajzomirkhan6@gmail.com>
2026-02-09 22:48:39 +0600

    first commit

---
+++ src/db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+
+
+async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+
+        console.log("database connected successfully")
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+
+module.exports = connectDB
